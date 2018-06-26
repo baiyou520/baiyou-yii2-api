1d51eae2e39b53d443c675a7b1d2f27f372bdb45
Billy Shen <likeboat@163.com>
2018-06-26 22:15:47 +0800

    多租户sid设计实现

--- common/components/ActiveRecord.php
+++ common/components/ActiveRecord.php
@@ -49,57 +49,5 @@ class ActiveRecord extends \yii\db\ActiveRecord
 
         return true;
     }
-
-    /**
-     * @see update()
-     * @param array $attributes attributes to update
-     * @return int|false the number of rows affected, or false if [[beforeSave()]] stops the updating process.
-     * @throws StaleObjectException
-     */
-    protected function updateInternal($attributes = null)
-    {
-        if (!$this->beforeSave(false)) {
-            return false;
-        }
-        $values = $this->getDirtyAttributes($attributes);
-
-        /**
-         * 复写区域 sft@caiyoudata.com
-         * ——————————————————————————————————————————————————————————————————————————————
-         * 绝大部分表需要在新增的时候表明数据是属于哪个实例，故在这里从cookies中得到sid，插入到数据库
-         */
-        $values['sid'] = Helper::getSid();
-
-
-        if (empty($values)) {
-            $this->afterSave(false, $values);
-            return 0;
-        }
-        $condition = $this->getOldPrimaryKey(true);
-        $lock = $this->optimisticLock();
-        if ($lock !== null) {
-            $values[$lock] = $this->$lock + 1;
-            $condition[$lock] = $this->$lock;
-        }
-        // We do not check the return value of updateAll() because it's possible
-        // that the UPDATE statement doesn't change anything and thus returns 0.
-        $rows = static::updateAll($values, $condition);
-
-        if ($lock !== null && !$rows) {
-            throw new StaleObjectException('The object being updated is outdated.');
-        }
-
-        if (isset($values[$lock])) {
-            $this->$lock = $values[$lock];
-        }
-
-        $changedAttributes = [];
-        foreach ($values as $name => $value) {
-            $changedAttributes[$name] = isset($this->_oldAttributes[$name]) ? $this->_oldAttributes[$name] : null;
-            $this->_oldAttributes[$name] = $value;
-        }
-        $this->afterSave(false, $changedAttributes);
-
-        return $rows;
-    }
+    
 }
