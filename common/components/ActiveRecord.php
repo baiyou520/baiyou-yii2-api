be8fdec14dbf733bcc14490e9cb9ab2f8d2477c6
Billy Shen <likeboat@163.com>
2018-06-28 16:52:07 +0800

    完善子系统的用户列表

--- common/components/ActiveRecord.php
+++ common/components/ActiveRecord.php
@@ -8,6 +8,11 @@
 
 namespace baiyou\common\components;
 
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveQuery;
+use yii\db\ActiveQueryInterface;
+use yii\helpers\ArrayHelper;
 
 class ActiveRecord extends \yii\db\ActiveRecord
 {
@@ -49,5 +54,63 @@ class ActiveRecord extends \yii\db\ActiveRecord
 
         return true;
     }
-    
+
+    /**
+     * 复写
+     * Finds ActiveRecord instance(s) by the given condition.
+     * This method is internally called by [[findOne()]] and [[findAll()]].
+     * @param mixed $condition please refer to [[findOne()]] for the explanation of this parameter
+     * @return ActiveQueryInterface the newly created [[ActiveQueryInterface|ActiveQuery]] instance.
+     * @throws InvalidConfigException if there is no primary key defined.
+     * @internal
+     */
+    protected static function findByCondition($condition)
+    {
+        $query = static::find();
+
+        if (!ArrayHelper::isAssociative($condition)) {
+            // query by primary key
+            $primaryKey = static::primaryKey();
+            if (isset($primaryKey[0])) {
+                $pk = $primaryKey[0];
+                if (!empty($query->join) || !empty($query->joinWith)) {
+                    $pk = static::tableName() . '.' . $pk;
+                }
+                // if condition is scalar, search for a single primary key, if it is array, search for multiple primary key values
+                $condition = [$pk => is_array($condition) ? array_values($condition) : $condition];
+            } else {
+                throw new InvalidConfigException('"' . get_called_class() . '" must have a primary key.');
+            }
+        } elseif (is_array($condition)) {
+            $condition = static::filterCondition($condition);
+        }
+
+        /**
+         * 复写区域 sft@caiyoudata.com
+         * ——————————————————————————————————————————————————————————————————————————————
+         * 绝大部分表需要在新增的时候表明数据是属于哪个实例，故在这里从cookies中得到sid，插入到数据库
+         */
+        $condition['sid'] = Helper::getSid();
+
+        return $query->andWhere($condition);
+    }
+
+    /**
+     * 复写
+     * {@inheritdoc}
+     * @return ActiveQuery the newly created [[ActiveQuery]] instance.
+     */
+    public static function find()
+    {
+        /**
+         * 复写区域 sft@caiyoudata.com
+         * ——————————————————————————————————————————————————————————————————————————————
+         * 绝大部分表需要在新增的时候表明数据是属于哪个实例，故在这里从cookies中得到sid，插入到数据库
+         */
+        return Yii::createObject(ActiveQuery::className(), [get_called_class()])->where(['sid' => Helper::getSid()]);
+
+//        return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
+    }
+
+
 }
