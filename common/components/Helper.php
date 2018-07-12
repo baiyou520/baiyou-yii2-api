39e013be49eee70d691c3adcf38e113ed4c19736
Billy Shen <likeboat@163.com>
2018-07-12 14:00:11 +0800

    增加helper方法

--- common/components/Helper.php
+++ common/components/Helper.php
@@ -73,5 +73,31 @@ class Helper
         curl_close($curl);
         return json_decode($output,true);
     }
+    /**发送验证码
+     * @param $phone
+     * @param $msg
+     * @return mixed
+     * @author xuy@caiyoudata.com
+     * @time  2018/6/25 15:21
+     */
+
+    public static function send_msg($phone,$msg){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://sms-api.luosimao.com/v1/send.json");
+
+        curl_setopt($ch, CURLOPT_HTTP_VERSION  , CURL_HTTP_VERSION_1_0 );
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 8);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_HTTPAUTH , CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD  , 'api:key-76b6e20f3c5fdc259204d174267ac2e4');
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array('mobile' => $phone,'message' =>$msg.'【百优应用】'));
+        $res = curl_exec( $ch );
+        curl_close( $ch );
+        return $res;
+    }
 
 }
