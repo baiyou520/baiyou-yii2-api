b1456bbd25e58b01d9cb5b552780180c72044c54
Billy Shen <likeboat@163.com>
2018-10-16 14:15:00 +0800

    获取客户性别等信息

--- common/migrations/m181016_021235_add_columns_customer.php
+++ common/migrations/m181016_021235_add_columns_customer.php
@@ -12,7 +12,7 @@ class m181016_021235_add_columns_customer extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn('customer', 'gender', 'TINYINT(3) NOT NULL DEFAULT 1 COMMENT "性别，1：男，2:女" AFTER `nickname`');
+        $this->addColumn('customer', 'gender', 'TINYINT(3) NOT NULL DEFAULT 0 COMMENT "性别，1：男，2:女,0:未知" AFTER `nickname`');
         $this->addColumn('customer', 'city', 'VARCHAR(30) NOT NULL DEFAULT "" COMMENT "城市" AFTER `nickname`');
         $this->addColumn('customer', 'country', 'VARCHAR(30) NOT NULL DEFAULT "" COMMENT "国家" AFTER `nickname`');
         $this->addColumn('customer', 'province', 'VARCHAR(30) NOT NULL DEFAULT "" COMMENT "省份" AFTER `nickname`');
