4ff15ca0975e6f398f48975200c06acadf65c403
Billy Shen <likeboat@163.com>
2019-01-10 15:52:58 +0800

    微信端也试用cookie认证

--- common/models/JwtModel.php
+++ common/models/JwtModel.php
@@ -98,7 +98,7 @@ class JwtModel extends \baiyou\common\components\ActiveRecord implements Identit
             'value' => $this->access_token,
             'domain' => '.baiyoudata.com',
             'httpOnly' => true,
-            'expire' => time() + 70 * 24 * 60 * 60, // 70天过期
+//            'expire' => time() + 70 * 24 * 60 * 60, // 70天过期  永不过期
         ]));
     }
 
